4a14f127e0e69099f3a2bf779cbc803bf53a04f3
797abhijeet <abhijeetsaxena615@gmail.com>
2025-12-25 22:18:45 +0530

    updated

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -127,7 +127,7 @@ export default function HomePage() {
 
         setSocket(newSocket)
       },
-      (error) => {
+      () => {
         setSearchingNearby(false)
         alert('Please enable location access to find nearby players')
       }
